59f6f5cf10eed8067ae5ab09f2a78ae8cf0c8c3a
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2013-06-02 21:56:01 +0200

    increase wall clock time measurement accuracy
    NTP is prone to time correction, so use System.nanoTime() (which is monotonic) instead of System.currentTimeMillis() (which granularity is not always great anyway) to measure wall clock time

--- src/main/java/clime/messadmin/filter/MessAdminThreadLocal.java
+++ src/main/java/clime/messadmin/filter/MessAdminThreadLocal.java
@@ -4,6 +4,7 @@
 package clime.messadmin.filter;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ThreadLocal containing some interesting data for internal usage.
@@ -11,8 +12,10 @@ import java.util.Date;
  * @author C&eacute;drik LIME
  */
 public class MessAdminThreadLocal {
-	private static final ThreadLocal/*<Date>*/ startTime = new ThreadLocal/*<Date>*/();
-	private static final ThreadLocal/*<Date>*/ stopTime = new ThreadLocal/*<Date>*/();
+	private static final ThreadLocal<Date> startTime = new ThreadLocal<Date>();
+	private static final ThreadLocal<Date> stopTime = new ThreadLocal<Date>();
+	private static final ThreadLocal<Long> startTimeNano = new ThreadLocal<Long>();
+	private static final ThreadLocal<Long> stopTimeNano = new ThreadLocal<Long>();
 
 	/**
 	 * 
@@ -23,34 +26,41 @@ public class MessAdminThreadLocal {
 
 	public static void start() {
 		startTime.set(new Date());
+		startTimeNano.set(System.nanoTime());
 	}
 
 	public static void stop() {
 		stopTime.set(new Date());
+		stopTimeNano.set(System.nanoTime());
 	}
 
 	public static void remove() {
-		//startTime.remove();
-		//stopTime.remove();
-		startTime.set(null);
-		stopTime.set(null);
+		startTime.remove();
+		stopTime.remove();
+		startTimeNano.remove();
+		stopTimeNano.remove();
 	}
 
 	public static Date getStartTime() {
-		Date date = (Date) startTime.get();
+		Date date = startTime.get();
 		// Workaround for bug of BEA Weblogic 8.1.5 / Java 1.4.2_05 / XP
 		return date != null ? date : new Date(0);
 	}
 
 	public static Date getStopTime() {
-		Date date = (Date) stopTime.get();
+		Date date = stopTime.get();
 		// Workaround for bug of BEA Weblogic 8.1.5 / Java 1.4.2_05 / XP
 		return date != null ? date : new Date(0);
 	}
 
+	/**
+	 * @return this request's wall clock time, in milliseconds
+	 */
 	public static int getUsedTime() {
 		try {
-			return (int) (getStopTime().getTime() - getStartTime().getTime());
+			//return (int) (getStopTime().getTime() - getStartTime().getTime()); // NTP is prone to time correction, so don't use System.currentTimeMillis()
+			long diffNano = stopTimeNano.get() - startTimeNano.get();
+			return (int) TimeUnit.NANOSECONDS.toMillis(diffNano);
 		} catch (NullPointerException npe) {
 			// Workaround for bug of BEA Weblogic 8.1.5 / Java 1.4.2_05 / XP
 			//e.printStackTrace();
